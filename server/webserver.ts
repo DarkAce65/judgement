ace9a9f8dae655e3f24350212de42bbbba91d4b8
DarkAce65 <tvdood99@gmail.com>
2020-12-27 14:56:06 -0500

    Add CORS middleware for local dev

--- server/webserver.ts
+++ server/webserver.ts
@@ -1,4 +1,4 @@
-import { opine } from "./deps.ts";
+import { opine, opineCors } from "./deps.ts";
 
 const BACKEND_PORT = Deno.env.get("BACKEND_PORT");
 if (BACKEND_PORT === undefined || isNaN(parseInt(BACKEND_PORT))) {
@@ -10,10 +10,15 @@ if (BACKEND_PORT === undefined || isNaN(parseInt(BACKEND_PORT))) {
 const port = parseInt(BACKEND_PORT);
 const app = opine();
 
+const APP_REQUEST_ORIGIN = Deno.env.get("APP_REQUEST_ORIGIN");
+if (APP_REQUEST_ORIGIN) {
+  app.use(opineCors({ origin: APP_REQUEST_ORIGIN }));
+}
+
 app.get("/hello", (req, res) => {
   res.json("Hello World");
 });
 
 app.listen({ port });
 
-console.log(`API running on port ${port}`);
+console.log(`API listening on port ${port}`);
