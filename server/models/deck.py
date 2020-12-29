1e7a2f1542e5736d65e7861ac9a2ba74fa6b70e2
DarkAce65 <tvdood99@gmail.com>
2020-12-29 17:42:49 -0500

    Add replace methods to deck

--- server/models/deck.py
+++ server/models/deck.py
@@ -1,15 +1,20 @@
+from __future__ import annotations
+
 import random
-from typing import List
+from collections import deque
+from collections.abc import Sequence
 
 from .card import Card, Suit
 
+NUM_CARDS_IN_DECK = 52
+
 
 class Decks:
-    cards: List[str]
+    cards: deque[str]
     num_decks: int
 
     def __init__(self, num_decks: int = 1) -> None:
-        self.cards = []
+        self.cards = deque(maxlen=num_decks * NUM_CARDS_IN_DECK)
         self.num_decks = num_decks
 
         for _ in range(num_decks):
@@ -27,12 +32,18 @@ class Decks:
             self.cards[shuffle_index] = self.cards[rand_index]
             self.cards[rand_index] = temp
 
-    def draw(self, count: int = 1) -> List[Card]:
-        drawn_cards = []
+    def draw(self, count: int = 1) -> deque[Card]:
+        drawn_cards: deque[Card] = deque(maxlen=count)
         for _ in range(count):
-            drawn_cards.append(Card.from_str(self.cards.pop()))
+            drawn_cards.append(Card.from_str(self.cards.popleft()))
 
         return drawn_cards
+
+    def replace_bottom(self, cards: Sequence[Card]) -> None:
+        self.cards.extend(map(str, cards))
+
+    def replace(self, cards: Sequence[Card]) -> None:
+        self.cards.extendleft(map(str, reversed(cards)))
 
     def __str__(self) -> str:
         return f"[{', '.join(map(str, self.cards))}]"
