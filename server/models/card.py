cb860bdfb62cee702fc313ff171cdf0dbaca32d2
DarkAce65 <tvdood99@gmail.com>
2020-12-29 14:04:58 -0500

    Add from_str and to_str methods to Card

--- server/models/card.py
+++ server/models/card.py
@@ -1,6 +1,9 @@
-from enum import Enum
+from __future__ import annotations
+
+from enum import Enum, unique
 
 
+@unique
 class Suit(Enum):
     DIAMONDS = "D"
     SPADES = "S"
@@ -14,20 +17,49 @@ class Card:
 
     def __init__(self, suit: Suit, number: int) -> None:
         if number < 2 or number > 14:
-            raise ValueError("number is out of bounds")
+            raise ValueError("Card number is out of bounds", number)
 
         self.suit = suit
         self.number = number
 
-    def __str__(self) -> str:
-        number = str(self.number)
-        if self.number == 14:
-            number = "A"
-        elif self.number == 13:
-            number = "K"
-        elif self.number == 12:
-            number = "Q"
-        elif self.number == 11:
-            number = "J"
+    @staticmethod
+    def from_str(card_string: str) -> Card:
+        if len(card_string) < 2:
+            raise ValueError("Invalid string to convert to Card", card_string)
 
-        return f"{self.suit.value}{number}"
+        suit_str = card_string[0]
+        number_str = card_string[1:]
+
+        try:
+            if number_str == "A":
+                number = 14
+            elif number_str == "K":
+                number = 13
+            elif number_str == "Q":
+                number = 12
+            elif number_str == "J":
+                number = 11
+            else:
+                number = int(number_str)
+        except ValueError as ex:
+            raise ValueError("Invalid number for Card", number_str) from ex
+
+        return Card(Suit(suit_str), number)
+
+    @staticmethod
+    def to_str(suit: Suit, number: int) -> str:
+        if number == 14:
+            number_str = "A"
+        elif number == 13:
+            number_str = "K"
+        elif number == 12:
+            number_str = "Q"
+        elif number == 11:
+            number_str = "J"
+        else:
+            number_str = str(number)
+
+        return f"{suit.value}{number_str}"
+
+    def __str__(self) -> str:
+        return Card.to_str(self.suit, self.number)
