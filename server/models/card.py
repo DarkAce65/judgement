80cfaf1ed2661824897695445f39583ce854deb8
DarkAce65 <tvdood99@gmail.com>
2020-12-29 16:43:08 -0500

    Rename number -> rank

--- server/models/card.py
+++ server/models/card.py
@@ -13,14 +13,14 @@ class Suit(Enum):
 
 class Card:
     suit: Suit
-    number: int
+    rank: int
 
-    def __init__(self, suit: Suit, number: int) -> None:
-        if number < 2 or number > 14:
-            raise ValueError("Card number is out of bounds", number)
+    def __init__(self, suit: Suit, rank: int) -> None:
+        if rank < 2 or rank > 14:
+            raise ValueError("Card rank is out of bounds", rank)
 
         self.suit = suit
-        self.number = number
+        self.rank = rank
 
     @staticmethod
     def from_str(card_string: str) -> Card:
@@ -28,41 +28,41 @@ class Card:
             raise ValueError("Invalid string to convert to Card", card_string)
 
         suit_str = card_string[0]
-        number_str = card_string[1:]
+        rank_str = card_string[1:]
 
         try:
-            if number_str == "A":
-                number = 14
-            elif number_str == "K":
-                number = 13
-            elif number_str == "Q":
-                number = 12
-            elif number_str == "J":
-                number = 11
+            if rank_str == "A":
+                rank = 14
+            elif rank_str == "K":
+                rank = 13
+            elif rank_str == "Q":
+                rank = 12
+            elif rank_str == "J":
+                rank = 11
             else:
-                number = int(number_str)
+                rank = int(rank_str)
         except ValueError as ex:
-            raise ValueError("Invalid number for Card", number_str) from ex
+            raise ValueError("Invalid rank for Card", rank_str) from ex
 
-        return Card(Suit(suit_str), number)
+        return Card(Suit(suit_str), rank)
 
     @staticmethod
-    def to_str(suit: Suit, number: int) -> str:
-        if number < 2 or number > 14:
-            raise ValueError("Card number is out of bounds", number)
+    def to_str(suit: Suit, rank: int) -> str:
+        if rank < 2 or rank > 14:
+            raise ValueError("Card rank is out of bounds", rank)
 
-        if number == 14:
-            number_str = "A"
-        elif number == 13:
-            number_str = "K"
-        elif number == 12:
-            number_str = "Q"
-        elif number == 11:
-            number_str = "J"
+        if rank == 14:
+            rank_str = "A"
+        elif rank == 13:
+            rank_str = "K"
+        elif rank == 12:
+            rank_str = "Q"
+        elif rank == 11:
+            rank_str = "J"
         else:
-            number_str = str(number)
+            rank_str = str(rank)
 
-        return f"{suit.value}{number_str}"
+        return f"{suit.value}{rank_str}"
 
     def __str__(self) -> str:
-        return Card.to_str(self.suit, self.number)
+        return Card.to_str(self.suit, self.rank)
