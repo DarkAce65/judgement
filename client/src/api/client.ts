5470eb615078b31d837c99b0b33dd68d8d33297b
DarkAce65 <tvdood99@gmail.com>
2020-12-30 22:45:50 -0500

    Refactor client functions to fetch/build backend URIs

--- client/src/api/client.ts
+++ client/src/api/client.ts
@@ -1,4 +1,27 @@
-const getAPIRoot = (): string => process.env.REACT_APP_API_ROOT ?? '/';
+export const getBackendHost = (): string | null =>
+  process.env.REACT_APP_BACKEND_HOST?.replace(/\/+$/, '') ?? null;
 
-export const fetchAPI = (path: string, init?: RequestInit): Promise<Response> =>
-  fetch(`${getAPIRoot()}${path}`, init);
+export const getAPIBase = (): string => {
+  const apiBase = [];
+  const backendHost = getBackendHost();
+  if (backendHost !== null && backendHost.length > 0) {
+    apiBase.push(backendHost);
+  }
+
+  const apiRoot = process.env.REACT_APP_API_ROOT?.replace(/^\/+/, '').replace(/\/+$/, '');
+  if (apiRoot && apiRoot.length > 0) {
+    apiBase.push(apiRoot);
+  }
+
+  return apiBase.join('/');
+};
+
+export const buildRequestPath = (path: string): string => {
+  const requestPath = [getAPIBase()];
+
+  if (path.length > 0) {
+    requestPath.push(path.replace(/^\/+/, ''));
+  }
+
+  return requestPath.join('/');
+};
