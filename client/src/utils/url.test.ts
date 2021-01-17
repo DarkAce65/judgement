3ae573ad3c0f4eecc29d406312877bf2d81e496f
DarkAce65 <tvdood99@gmail.com>
2021-01-17 01:46:45 -0500

    Create url joining utility with tests

---
+++ client/src/utils/url.test.ts
@@ -0,0 +1,229 @@
+import { join } from './url';
+
+describe('empty segments', () => {
+  it('joins empty segments', () => {
+    expect(join('')).toBe('');
+    expect(join('', '')).toBe('');
+    expect(join('', '', '')).toBe('');
+  });
+
+  describe('slashes and empty segments', () => {
+    const segments = ['', '/', '//', '///'];
+
+    describe('one component', () => {
+      const tests = [];
+      for (const segment1 of segments) {
+        tests.push([segment1]);
+      }
+
+      it.each(tests)('join(%p)', (segment1: string) => {
+        let expected = '/';
+        if (segment1.length === 0) {
+          expected = '';
+        } else if ([segment1].filter((s) => s.length > 0)[0].length >= 2) {
+          expected = '//';
+        }
+
+        expect(join(segment1)).toBe(expected);
+      });
+    });
+
+    describe('two components', () => {
+      const tests = [];
+      for (const segment1 of segments) {
+        for (const segment2 of segments) {
+          tests.push([segment1, segment2]);
+        }
+      }
+
+      it.each(tests)('join(%p, %p)', (segment1: string, segment2: string) => {
+        let expected = '/';
+        if ((segment1 + segment2).length === 0) {
+          expected = '';
+        } else if ([segment1, segment2].filter((s) => s.length > 0)[0].length >= 2) {
+          expected = '//';
+        }
+
+        expect(join(segment1, segment2)).toBe(expected);
+      });
+    });
+
+    describe('three components', () => {
+      const tests = [];
+      for (const segment1 of segments) {
+        for (const segment2 of segments) {
+          for (const segment3 of segments) {
+            tests.push([segment1, segment2, segment3]);
+          }
+        }
+      }
+
+      it.each(tests)('join(%p, %p, %p)', (segment1: string, segment2: string, segment3: string) => {
+        let expected = '/';
+        if ((segment1 + segment2 + segment3).length === 0) {
+          expected = '';
+        } else if ([segment1, segment2, segment3].filter((s) => s.length > 0)[0].length >= 2) {
+          expected = '//';
+        }
+
+        expect(join(segment1, segment2, segment3)).toBe(expected);
+      });
+    });
+  });
+
+  it('joins a non-empty segment with empty segments', () => {
+    expect(join('a', '')).toBe('a');
+    expect(join('/a', '')).toBe('/a');
+    expect(join('a/', '')).toBe('a/');
+    expect(join('/a/', '')).toBe('/a/');
+
+    expect(join('', 'a')).toBe('a');
+    expect(join('', '/a')).toBe('/a');
+    expect(join('', 'a/')).toBe('a/');
+    expect(join('', '/a/')).toBe('/a/');
+
+    expect(join('a', '', '')).toBe('a');
+    expect(join('/a', '', '')).toBe('/a');
+    expect(join('a/', '', '')).toBe('a/');
+    expect(join('/a/', '', '')).toBe('/a/');
+
+    expect(join('', 'a', '')).toBe('a');
+    expect(join('', '/a', '')).toBe('/a');
+    expect(join('', 'a/', '')).toBe('a/');
+    expect(join('', '/a/', '')).toBe('/a/');
+
+    expect(join('', '', 'a')).toBe('a');
+    expect(join('', '', '/a')).toBe('/a');
+    expect(join('', '', 'a/')).toBe('a/');
+    expect(join('', '', '/a/')).toBe('/a/');
+  });
+});
+
+it('joins a non-empty segment with slashes', () => {
+  expect(join('a', '/')).toBe('a/');
+  expect(join('/a', '/')).toBe('/a/');
+  expect(join('a/', '/')).toBe('a/');
+  expect(join('/a/', '/')).toBe('/a/');
+
+  expect(join('/', 'a')).toBe('/a');
+  expect(join('/', '/a')).toBe('/a');
+  expect(join('/', 'a/')).toBe('/a/');
+  expect(join('/', '/a/')).toBe('/a/');
+
+  expect(join('a', '/', '/')).toBe('a/');
+  expect(join('/a', '/', '/')).toBe('/a/');
+  expect(join('a/', '/', '/')).toBe('a/');
+  expect(join('/a/', '/', '/')).toBe('/a/');
+
+  expect(join('/', 'a', '/')).toBe('/a/');
+  expect(join('/', '/a', '/')).toBe('/a/');
+  expect(join('/', 'a/', '/')).toBe('/a/');
+  expect(join('/', '/a/', '/')).toBe('/a/');
+
+  expect(join('/', '/', 'a')).toBe('/a');
+  expect(join('/', '/', '/a')).toBe('/a');
+  expect(join('/', '/', 'a/')).toBe('/a/');
+  expect(join('/', '/', '/a/')).toBe('/a/');
+});
+
+describe('non-empty segments', () => {
+  const firstSegments = ['a', '/a', 'a/', '/a/'];
+  const secondSegments = ['b', '/b', 'b/', '/b/'];
+  const thirdSegments = ['c', '/c', 'c/', '/c/'];
+
+  describe('one segment', () => {
+    const tests = [];
+    for (const segment1 of firstSegments) {
+      const expected = segment1;
+      tests.push([segment1, expected]);
+    }
+
+    it.each(tests)('join(%p)', (segment1: string, expected: string) => {
+      expect(join(segment1)).toBe(expected);
+    });
+  });
+
+  describe('two segments', () => {
+    const tests = [];
+    for (const segment1 of firstSegments) {
+      for (const segment2 of secondSegments) {
+        let expected = 'a/b';
+        if (segment1.startsWith('/')) {
+          expected = `/${expected}`;
+        }
+        if (segment2.endsWith('/')) {
+          expected += '/';
+        }
+
+        tests.push([segment1, segment2, expected]);
+      }
+    }
+
+    it.each(tests)('join(%p, %p)', (segment1: string, segment2: string, expected: string) => {
+      expect(join(segment1, segment2)).toBe(expected);
+    });
+  });
+
+  describe('three segments', () => {
+    const tests = [];
+    for (const segment1 of firstSegments) {
+      for (const segment2 of secondSegments) {
+        for (const segment3 of thirdSegments) {
+          let expected = 'a/b/c';
+          if (segment1.startsWith('/')) {
+            expected = `/${expected}`;
+          }
+          if (segment3.endsWith('/')) {
+            expected += '/';
+          }
+
+          tests.push([segment1, segment2, segment3, expected]);
+        }
+      }
+    }
+
+    it.each(tests)(
+      'join(%p, %p, %p)',
+      (segment1: string, segment2: string, segment3: string, expected: string) => {
+        expect(join(segment1, segment2, segment3)).toBe(expected);
+      }
+    );
+  });
+});
+
+it('tests edge cases for joining', () => {
+  expect(join()).toBe('');
+
+  expect(join('', '/', '/a')).toBe('/a');
+
+  expect(join('//test.domain.com')).toBe('//test.domain.com');
+  expect(join('//', 'test.domain.com')).toBe('//test.domain.com');
+
+  expect(join('file:')).toBe('file://');
+  expect(join('file:/')).toBe('file://');
+  expect(join('file:///')).toBe('file:///');
+  expect(join('file:etc')).toBe('file://etc');
+  expect(join('file:', 'etc', 'fstab')).toBe('file://etc/fstab');
+  expect(join('file://', 'etc', 'fstab')).toBe('file://etc/fstab');
+  expect(join('file://', '/etc', 'fstab')).toBe('file:///etc/fstab');
+  expect(join('file://test', '/etc', 'fstab')).toBe('file://test/etc/fstab');
+  expect(join('file:///', 'etc', 'fstab')).toBe('file:///etc/fstab');
+  expect(join('file:///', 'etc/fstab')).toBe('file:///etc/fstab');
+  expect(join('file:', '///etc//fstab')).toBe('file:///etc/fstab');
+
+  expect(() => join('http:')).toThrow();
+  expect(join('http:', 'test.domain.com')).toBe('http://test.domain.com');
+  expect(join('http://', 'test.domain.com')).toBe('http://test.domain.com');
+  expect(join('http:///', 'test.domain.com')).toBe('http://test.domain.com');
+  expect(join('http://', '//test.domain.com')).toBe('http://test.domain.com');
+  expect(join('http:/', '/test.domain.com')).toBe('http://test.domain.com');
+  expect(join('http:', '//test.domain.com')).toBe('http://test.domain.com');
+  expect(join('http:', 'test.domain.com', 'api')).toBe('http://test.domain.com/api');
+
+  expect(join('a', 'b//c')).toBe('a/b/c');
+  expect(join('a', '//b//c')).toBe('a/b/c');
+  expect(join('a', 'b//c//')).toBe('a/b/c/');
+  expect(join('a', '//b//c//')).toBe('a/b/c/');
+
+  expect(join('////a', 'b//c')).toBe('//a/b/c');
+});
---
+++ client/src/utils/url.ts
@@ -0,0 +1,87 @@
+const normalizeProtocolSegment = (protocolSegment: string): string => {
+  if (protocolSegment.startsWith(':') || protocolSegment.startsWith('/')) {
+    throw new Error(`Malformed protocol segment: ${protocolSegment}`);
+  }
+
+  const [protocol, afterProtocol, ...extraParts] = protocolSegment.split(':');
+
+  if (extraParts.length !== 0) {
+    throw new Error(`Malformed protocol segment: ${protocolSegment}`);
+  }
+
+  let normalizedSegment = `${protocol}:`;
+
+  let slashes = 0;
+  let c = 0;
+  for (; c < afterProtocol.length; c++) {
+    const char = afterProtocol[c];
+    if (char !== '/') {
+      break;
+    }
+
+    slashes++;
+    if ((protocol === 'file' && slashes > 3) || (protocol !== 'file' && slashes > 2)) {
+      continue;
+    }
+
+    normalizedSegment += char;
+  }
+  while (slashes < 2) {
+    normalizedSegment += '/';
+    slashes++;
+  }
+  normalizedSegment += afterProtocol.slice(c);
+
+  return normalizedSegment;
+};
+
+export const join = (...segments: string[]): string => {
+  let url = '';
+
+  for (let i = 0; i < segments.length; i++) {
+    const segment = segments[i];
+
+    if (segment.length === 0) {
+      continue;
+    }
+
+    if (segment.indexOf(':') !== -1) {
+      if (url.length !== 0) {
+        throw new Error(`Unexpected protocol segment: ${segment}`);
+      }
+
+      url += normalizeProtocolSegment(segment);
+
+      continue;
+    }
+
+    let normalizedSegment = segment.replaceAll(/([^\/]+)\/+/g, '$1/');
+    if (url.length === 0) {
+      normalizedSegment = normalizedSegment.replace(/^\/{2,}/, '//');
+    } else {
+      normalizedSegment = normalizedSegment.replace(/^\/+/, '/');
+    }
+
+    if (url === 'file://') {
+      url += normalizedSegment;
+    } else if (url.endsWith('/') && normalizedSegment.startsWith('/')) {
+      url += normalizedSegment.replace(/^\/+/, '');
+    } else if (url.endsWith('/') || normalizedSegment.startsWith('/')) {
+      url += normalizedSegment;
+    } else {
+      if (url.length === 0) {
+        url = normalizedSegment;
+      } else {
+        url += `/${normalizedSegment}`;
+      }
+    }
+  }
+
+  if (url !== 'file://' && /^[^\/]+:\/{2}$/.test(url)) {
+    throw new Error(
+      `Malformed url from segments: [${segments.map((segment) => `'${segment}'`).join(', ')}]`
+    );
+  }
+
+  return url;
+};
