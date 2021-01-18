0d0752c9b7342a53e0c74729ba7ee2042b298262
DarkAce65 <tvdood99@gmail.com>
2021-01-18 18:51:17 -0500

    Add unit tests for cards

---
+++ server/tests/test_cards.py
@@ -0,0 +1,30 @@
+from unittest.case import TestCase
+
+from models.card import Card, Suit
+
+
+class TestCards(TestCase):
+    def test_creating_a_card(self):
+        ace_of_spades = Card(Suit.SPADES, 1)
+        self.assertEqual(ace_of_spades.suit, Suit.SPADES)
+        self.assertEqual(ace_of_spades.rank, 1)
+
+        eight_of_hearts = Card(Suit.HEARTS, 8)
+        self.assertEqual(eight_of_hearts.suit, Suit.HEARTS)
+        self.assertEqual(eight_of_hearts.rank, 8)
+
+    def test_creating_a_card_from_string(self):
+        ace_of_spades = Card.from_str("SA")
+        self.assertEqual(ace_of_spades.suit, Suit.SPADES)
+        self.assertEqual(ace_of_spades.rank, 1)
+
+        eight_of_hearts = Card.from_str("H8")
+        self.assertEqual(eight_of_hearts.suit, Suit.HEARTS)
+        self.assertEqual(eight_of_hearts.rank, 8)
+
+    def test_converting_card_to_string(self):
+        ace_of_spades = Card(Suit.SPADES, 1)
+        self.assertEqual(str(ace_of_spades), "SA")
+
+        eight_of_hearts = Card(Suit.HEARTS, 8)
+        self.assertEqual(str(eight_of_hearts), "H8")
