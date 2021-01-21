6d19718b24115f291d0bf15dcd562930f684fc1a
DarkAce65 <tvdood99@gmail.com>
2021-01-21 01:02:34 -0500

    Lock redis server with password using docker secrets

--- server/main.py
+++ server/main.py
@@ -7,7 +7,16 @@ from redis import Redis
 from socketio import ASGIApp, AsyncServer
 from starlette.responses import Response
 
-redis_client = Redis(host="redis")
+if "REDIS_PASSWORD_FILE" in os.environ:
+    redis_password_file = os.environ.get("REDIS_PASSWORD_FILE")
+    if redis_password_file is None:
+        raise Exception("Missing path to redis password secret")
+
+    with open(redis_password_file) as f:
+        redis_password = f.readline().strip()
+        redis_client = Redis(host="redis", password=redis_password)
+else:
+    redis_client = Redis(host="redis")
 
 app = FastAPI()
 
