2fcce8f0db1b41d9c06cbdd14d0d9323fdb97295
DarkAce65 <tvdood99@gmail.com>
2021-01-31 19:02:58 -0500

    Convert request and response bodies to camel case

--- server/server/models/requests.py
+++ server/server/models/requests.py
@@ -1,5 +1,7 @@
-from pydantic import BaseModel, Field
+from pydantic import Field
+
+from server.models.camel_model import CamelModel
 
 
-class CreateGameRequest(BaseModel):
+class CreateGameRequest(CamelModel):
     player_name: str = Field(None, title="The name of the player")
--- server/server/models/responses.py
+++ server/server/models/responses.py
@@ -1,5 +1,7 @@
-from pydantic import BaseModel, Field
+from pydantic import Field
+
+from server.models.camel_model import CamelModel
 
 
-class GameResponse(BaseModel):
+class GameResponse(CamelModel):
     room_id: str = Field(None, title="The room id of the game")
