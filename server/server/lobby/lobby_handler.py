b50955b432e39bd2436ea8d162a19086ffb81cac
DarkAce65 <tvdood99@gmail.com>
2021-02-07 01:38:41 -0500

    Drop self parameter

--- server/server/lobby/lobby_handler.py
+++ server/server/lobby/lobby_handler.py
@@ -7,59 +7,56 @@ active_players: dict[str, Player] = {}
 rooms: dict[str, Room] = {}
 
 
-def get_player(self, player_id: str) -> Player:
-    if player_id not in self.active_players:
+def get_player(player_id: str) -> Player:
+    if player_id not in active_players:
         raise ValueError(f"Invalid player id: {player_id}")
 
-    return self.active_players[player_id]
+    return active_players[player_id]
 
 
-def get_room(self, room_id: str) -> Room:
-    if room_id not in self.rooms:
+def get_room(room_id: str) -> Room:
+    if room_id not in rooms:
         raise ValueError(f"Invalid room id: {room_id}")
 
-    return self.rooms[room_id]
+    return rooms[room_id]
 
 
-def ensure_player_with_name(
-    self, player_name: str, player_id: Optional[str] = None
-) -> Player:
-    if player_id is not None and player_id in self.active_players:
-        player = self.get_player(player_id)
+def ensure_player_with_name(player_name: str, player_id: Optional[str] = None) -> Player:
+    if player_id is not None and player_id in active_players:
+        player = get_player(player_id)
         player.name = player_name
         return player
 
     player = Player(player_name)
 
-    self.active_players[player.player_id] = player
+    active_players[player.player_id] = player
     return player
 
 
-def create_room(self, host_id: str) -> Room:
-    player = self.get_player(host_id)
+def create_room(host_id: str) -> Room:
+    player = get_player(host_id)
 
     room_id = Room.generate_id()
-    while room_id in self.rooms:
+    while room_id in rooms:
         room_id = Room.generate_id()
 
     room = Room(room_id, host_id)
-    self.rooms[room_id] = room
+    rooms[room_id] = room
 
     player.num_joined_rooms += 1
 
     return room
 
 
-def get_players_in_room(self, room_id: str) -> dict[str, Player]:
+def get_players_in_room(room_id: str) -> dict[str, Player]:
     return {
-        player_id: self.get_player(player_id)
-        for player_id in self.get_room(room_id).player_ids
+        player_id: get_player(player_id) for player_id in get_room(room_id).player_ids
     }
 
 
-def add_player_to_room(self, room_id: str, player_id: str) -> Room:
-    player = self.get_player(player_id)
-    room = self.get_room(room_id)
+def add_player_to_room(room_id: str, player_id: str) -> Room:
+    player = get_player(player_id)
+    room = get_room(room_id)
 
     if room.add_player(player_id):
         player.num_joined_rooms += 1
@@ -67,9 +64,9 @@ def add_player_to_room(self, room_id: str, player_id: str) -> Room:
     return room
 
 
-def drop_player_from_room(self, room_id: str, player_id: str) -> bool:
-    player = self.get_player(player_id)
-    room = self.get_room(room_id)
+def drop_player_from_room(room_id: str, player_id: str) -> bool:
+    player = get_player(player_id)
+    room = get_room(room_id)
 
     if not room.remove_player(player_id):
         return False
@@ -77,6 +74,6 @@ def drop_player_from_room(self, room_id: str, player_id: str) -> bool:
     player.num_joined_rooms -= 1
 
     if len(room.player_ids) == 0:
-        del self.rooms[room.room_id]
+        del rooms[room.room_id]
 
     return True
