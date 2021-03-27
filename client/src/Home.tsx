9cdafb3c01693752fbe01957727de1384f55f4b3
DarkAce65 <tvdood99@gmail.com>
2021-03-27 14:25:21 -0400

    Wrap content in Layout

--- client/src/Home.tsx
+++ client/src/Home.tsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
 import { ArrowRightOutlined } from '@ant-design/icons';
-import { Button, Form, Input, notification } from 'antd';
+import { Button, Form, Input, Layout, notification } from 'antd';
 
 import { fetchAPI } from './api/client';
 
@@ -9,40 +9,43 @@ const Home = () => {
   const [loading, setLoading] = useState(false);
   const [playerName, setPlayerName] = useState(localStorage.getItem('playerName') || '');
 
-  return (
-    <Form layout="inline">
-      <Form.Item label="Name">
-        <Input
-          value={playerName}
-          onChange={({ target: { value } }) => {
-            setPlayerName(value);
-          }}
-        />
-      </Form.Item>
-      <Form.Item>
-        <Button
-          loading={loading ? { delay: 100 } : false}
-          icon={<ArrowRightOutlined />}
-          onClick={() => {
-            setLoading(true);
+  const handlePlayerNameChange = useCallback(() => {
+    setLoading(true);
 
-            fetchAPI('/ensure-player', {
-              method: 'POST',
-              body: JSON.stringify({ playerName }),
-            })
-              .then(() => {
-                localStorage.setItem('playerName', playerName);
-              })
-              .catch(() => {
-                notification.error({ message: 'Failed to set name', duration: 0 });
-              })
-              .then(() => {
-                setLoading(false);
-              });
-          }}
-        />
-      </Form.Item>
-    </Form>
+    fetchAPI('/ensure-player', { method: 'POST', body: JSON.stringify({ playerName }) })
+      .then(() => {
+        localStorage.setItem('playerName', playerName);
+      })
+      .catch(() => {
+        notification.error({ message: 'Failed to set name', duration: 0 });
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [playerName]);
+
+  return (
+    <Layout>
+      <Layout.Content style={{ padding: 24 }}>
+        <Form layout="inline">
+          <Form.Item label="Name">
+            <Input
+              value={playerName}
+              onChange={({ target: { value } }) => {
+                setPlayerName(value);
+              }}
+            />
+          </Form.Item>
+          <Form.Item>
+            <Button
+              loading={loading ? { delay: 100 } : false}
+              icon={<ArrowRightOutlined />}
+              onClick={handlePlayerNameChange}
+            />
+          </Form.Item>
+        </Form>
+      </Layout.Content>
+    </Layout>
   );
 };
 
