205b5830d6b55a6a89639fe966d95dcd3e65c8fb
DarkAce65 <tvdood99@gmail.com>
2021-04-02 22:26:28 -0400

    Create useFetch hook

---
+++ client/src/api/FetchStatus.ts
@@ -0,0 +1,1 @@
+export type FetchStatus = 'uninitialized' | 'pending' | 'succeeded' | 'failed';
---
+++ client/src/api/useFetch.ts
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react';
+
+import { FetchStatus } from './FetchStatus';
+import { fetchAPI } from './client';
+
+const useFetch = (...args: Parameters<typeof fetchAPI>) => {
+  const [status, setStatus] = useState<FetchStatus>('uninitialized');
+  const [response, setResponse] = useState<Response | null>(null);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [error, setError] = useState<any>(null);
+
+  useEffect(() => {
+    setStatus('pending');
+    setResponse(null);
+    setError(null);
+
+    fetchAPI(...args)
+      .then((r) => r.json())
+      .then((r) => {
+        setResponse(r);
+        setStatus('succeeded');
+      })
+      .catch((e) => {
+        setError(e);
+        setStatus('failed');
+      });
+  }, [...args]); // eslint-disable-line react-hooks/exhaustive-deps
+
+  return { status, response, error };
+};
+
+export default useFetch;
