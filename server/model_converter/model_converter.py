bdbb4a0b0426125a253c45ba6c6ce7db971ac7d5
DarkAce65 <tvdood99@gmail.com>
2021-04-18 18:43:33 -0400

    Clean up watcher and generator

--- server/model_converter/model_converter.py
+++ server/model_converter/model_converter.py
@@ -1,25 +1,62 @@
+import argparse
+import logging
+import subprocess
 from pathlib import Path, PurePath
 
 from pydantic2ts import generate_typescript_defs
 
-MODEL_FILENAMES = ["requests", "responses", "websocket"]
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s] %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+logging.getLogger("pydantic2ts").propagate = False
+
+logger = logging.getLogger(__name__)
 
 
+MODEL_MODULES = ["requests", "responses", "websocket"]
+
+
+project_root = Path().resolve().parent
 current_dir = Path(__file__).parent
 json2ts_path = PurePath(current_dir / "node_modules/.bin/json2ts")
 
 
 def yarn_install() -> None:
-    pass
+    subprocess.run(
+        ["yarn", "install", "--silent", "--check-files"], check=True, cwd=current_dir
+    )
 
 
 def generate(module_name: str) -> None:
     out_filename = PurePath(current_dir / module_name).with_suffix(".ts")
-    generate_typescript_defs(f"server.models.{module_name}", out_filename, json2ts_path)
+    try:
+        generate_typescript_defs(
+            f"server.models.{module_name}", out_filename, json2ts_path
+        )
+
+        logger.info(
+            "Wrote interfaces for server.models.%s to %s",
+            module_name,
+            out_filename.relative_to(project_root),
+        )
+    except:  # pylint: disable=bare-except
+        logger.exception("Failed to process models in server.models.%s", module_name)
 
 
 if __name__ == "__main__":
-    yarn_install()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--skip-yarn", action="store_true")
+    parser.add_argument("--module", action="append", dest="modules")
+    args = parser.parse_args()
 
-    for module_name in MODEL_FILENAMES:
+    module_names = args.modules
+    if module_names is None:
+        module_names = MODEL_MODULES
+
+    if not args.skip_yarn:
+        yarn_install()
+
+    for module_name in args.modules:
         generate(module_name)
--- server/model_converter/watcher.py
+++ server/model_converter/watcher.py
@@ -1,28 +1,39 @@
+import logging
 import subprocess
 from pathlib import Path, PurePath
 
 from watchgod import watch
 from watchgod.watcher import RegExpWatcher
 
-from model_converter.model_converter import MODEL_FILENAMES, yarn_install
+from model_converter.model_converter import MODEL_MODULES, yarn_install
+
+
+def run_generator(model_modules: list[str]) -> None:
+    subprocess.run(
+        [
+            "python",
+            "-m",
+            "model_converter.model_converter",
+            "--skip-yarn",
+            *[arg for module in model_modules for arg in ("--module", module)],
+        ],
+        check=True,
+    )
+
 
 if __name__ == "__main__":
     yarn_install()
 
-    model_filenames_to_watch = "|".join(MODEL_FILENAMES)
+    watch_dir = (Path(__file__).parent / "../server/models").resolve()
 
+    logging.info("Generating models in %s...", watch_dir)
+    run_generator(MODEL_MODULES)
+
+    logging.info("Watching for changes in %s...", watch_dir)
+    WATCHER_FILE_REGEX = r"^.*(" + "|".join(MODEL_MODULES) + r")\.py$"
     for changes in watch(
-        (Path(__file__).parent / "../server/models").resolve(),
+        watch_dir,
         watcher_cls=RegExpWatcher,
-        watcher_kwargs=dict(re_files=r"^.*(" + model_filenames_to_watch + r")\.py$"),
+        watcher_kwargs=dict(re_files=WATCHER_FILE_REGEX),
     ):
-        for change in changes:
-            (_, changed_file) = change
-            changed_module_name = PurePath(changed_file).stem
-            subprocess.run(
-                [
-                    "python",
-                    "-c",
-                    f'from model_converter.model_converter import generate; generate("{changed_module_name}")',
-                ]
-            )
+        run_generator([PurePath(change[1]).stem for change in changes])
