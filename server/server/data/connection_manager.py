df8e459f691abc08617cecc7e63c29e29dc00bf2
DarkAce65 <tvdood99@gmail.com>
2021-04-24 14:25:27 -0400

    Create bidirectional mapping helper class

--- server/server/data/connection_manager.py
+++ server/server/data/connection_manager.py
@@ -1,8 +1,48 @@
+from collections import defaultdict
+
+from server.data import room_manager
 from server.sio_app import sio
 
 from . import player_manager, socket_messager
 
-player_id_to_client_ids: dict[str, set[str]] = {}
+
+class PlayerClientIdMapping:
+    player_id_to_client_ids: defaultdict[str, set[str]]
+    client_id_to_player_id: dict[str, str]
+
+    def __init__(self) -> None:
+        self.player_id_to_client_ids = defaultdict(set)
+        self.client_id_to_player_id = {}
+
+    def get_client_ids_for_player(self, player_id: str) -> set[str]:
+        return self.player_id_to_client_ids.get(player_id, set())
+
+    def get_player_id_for_client(self, client_id: str) -> str:
+        return self.client_id_to_player_id[client_id]
+
+    def add_pair(self, player_id: str, client_id: str) -> None:
+        self.player_id_to_client_ids[player_id].add(client_id)
+        self.client_id_to_player_id[client_id] = player_id
+
+    def remove_pair(self, player_id: str, client_id: str) -> None:
+        self.player_id_to_client_ids[player_id].remove(client_id)
+        del self.client_id_to_player_id[client_id]
+
+    def remove_player_id(self, player_id: str) -> None:
+        for client_id in self.get_client_ids_for_player(player_id):
+            del self.client_id_to_player_id[client_id]
+
+        del self.player_id_to_client_ids[player_id]
+
+    def remove_client_id(self, client_id: str) -> None:
+        if client_id in self.client_id_to_player_id:
+            player_id = self.client_id_to_player_id[client_id]
+            self.player_id_to_client_ids[player_id].remove(client_id)
+
+        del self.client_id_to_player_id[client_id]
+
+
+player_client_mapping = PlayerClientIdMapping()
 client_id_to_room_id: dict[str, str] = {}
 
 
@@ -10,7 +50,7 @@ def get_client_ids_for_player(player_id: str) -> set[str]:
     if not player_manager.player_exists(player_id):
         raise ValueError(f"Invalid player id: {player_id}")
 
-    return player_id_to_client_ids.get(player_id, set())
+    return player_client_mapping.get_client_ids_for_player(player_id)
 
 
 def get_client_ids_for_players(player_ids: set[str]) -> set[str]:
@@ -24,10 +64,7 @@ def get_client_ids_for_players(player_ids: set[str]) -> set[str]:
 
 
 def connect_player_client(player_id: str, client_id: str) -> None:
-    if player_id not in player_id_to_client_ids:
-        player_id_to_client_ids[player_id] = set()
-
-    player_id_to_client_ids[player_id].add(client_id)
+    player_client_mapping.add_pair(player_id, client_id)
     sio.enter_room(client_id, player_id)
 
 
@@ -41,6 +78,11 @@ async def propagate_name_change(player_id: str) -> None:
 
 
 def add_player_client_to_room(client_id: str, room_id: str) -> None:
+    player_id = player_client_mapping.get_player_id_for_client(client_id)
+
+    if not room_manager.get_room(room_id).has_player(player_id):
+        room_manager.add_player_to_room(player_id, room_id)
+
     if client_id in client_id_to_room_id:
         sio.leave_room(client_id, client_id_to_room_id[client_id])
 
@@ -48,12 +90,21 @@ def add_player_client_to_room(client_id: str, room_id: str) -> None:
     client_id_to_room_id[client_id] = room_id
 
 
+def remove_player_client_from_room(client_id: str, room_id: str) -> None:
+    player_id = player_client_mapping.get_player_id_for_client(client_id)
+
+    if room_manager.get_room(room_id).has_player(player_id):
+        room_manager.add_player_to_room(player_id, room_id)
+
+        if client_id in client_id_to_room_id:
+            sio.leave_room(client_id, client_id_to_room_id[client_id])
+            room_manager.drop_player_from_room(player_id, room_id)
+
+            del client_id_to_room_id[client_id]
+
+
 def disconnect_player_client(player_id: str, client_id: str) -> None:
-    if (
-        player_id in player_id_to_client_ids
-        and client_id in player_id_to_client_ids[player_id]
-    ):
-        player_id_to_client_ids[player_id].remove(client_id)
+    player_client_mapping.remove_pair(player_id, client_id)
 
     if client_id in client_id_to_room_id:
         del client_id_to_room_id[client_id]
