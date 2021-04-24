8b810a6a5d8857468a6c1890b4cdf0240e07e1d2
DarkAce65 <tvdood99@gmail.com>
2021-04-24 16:19:05 -0400

    Rename sid -> client_id

--- server/server/api/websocket.py
+++ server/server/api/websocket.py
@@ -14,8 +14,8 @@ F = TypeVar("F", bound=Callable[..., Any])
 
 def require_player(socket_handler: F) -> F:
     @functools.wraps(socket_handler)
-    async def wrapper(sid: str, *args: Any, **kwargs: Any) -> Any:
-        session = await sio.get_session(sid)
+    async def wrapper(client_id: str, *args: Any, **kwargs: Any) -> Any:
+        session = await sio.get_session(client_id)
         player_id = session["player_id"]
 
         try:
@@ -24,7 +24,7 @@ def require_player(socket_handler: F) -> F:
             player = None
 
         if player is None:
-            await sio.disconnect(sid)
+            await sio.disconnect(client_id)
             return
 
         for argname, annotation in inspect.getfullargspec(
@@ -34,36 +34,36 @@ def require_player(socket_handler: F) -> F:
                 kwargs[argname] = player
                 break
 
-        return await socket_handler(sid, *args, **kwargs)
+        return await socket_handler(client_id, *args, **kwargs)
 
     return cast(F, wrapper)
 
 
 @sio.on("connect")
-async def connect(sid: str, _environ: dict, auth: dict) -> None:
+async def connect(client_id: str, _environ: dict, auth: dict) -> None:
     player_id = None if auth is None else auth["player_id"]
 
     if player_id is None or not player_manager.player_exists(player_id):
         raise ConnectionRefusedError("unknown_player_id")
 
-    connection_manager.connect_player_client(player_id, sid)
+    connection_manager.connect_player_client(player_id, client_id)
 
-    await sio.save_session(sid, {"player_id": player_id})
+    await sio.save_session(client_id, {"player_id": player_id})
     await sio.emit(
-        "client_connect", StringMessage(data="Client connected: " + sid).dict()
+        "client_connect", StringMessage(data="Client connected: " + client_id).dict()
     )
 
 
 @sio.on("join_room")
 @require_player
-async def handle_join_room(sid: str, room_id: str) -> None:
-    connection_manager.add_player_client_to_room(sid, room_id)
+async def handle_join_room(client_id: str, room_id: str) -> None:
+    connection_manager.add_player_client_to_room(client_id, room_id)
     await socket_messager.emit_players(room_id)
 
 
 @sio.on("message")
 @require_player
-async def handle_message(_sid: str, message: str, player: Player) -> None:
+async def handle_message(_client_id: str, message: str, player: Player) -> None:
     await sio.send(message, to=player.player_id)
 
     sender = player.player_id if player.name is None else player.name
@@ -74,8 +74,9 @@ async def handle_message(_sid: str, message: str, player: Player) -> None:
 
 
 @sio.on("disconnect")
-async def disconnect(sid: str) -> None:
-    connection_manager.disconnect_player_client(sid)
+async def disconnect(client_id: str) -> None:
+    connection_manager.disconnect_player_client(client_id)
     await sio.emit(
-        "client_disconnect", StringMessage(data="Client disconnected: " + sid).dict()
+        "client_disconnect",
+        StringMessage(data="Client disconnected: " + client_id).dict(),
     )
