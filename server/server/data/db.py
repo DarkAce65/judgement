41930127c86840386932e27fcb4183293fadc12e
DarkAce65 <tvdood99@gmail.com>
2021-06-26 22:58:53 -0400

    Move db insertion and update methods into db.py

--- server/server/data/db.py
+++ server/server/data/db.py
@@ -1,4 +1,5 @@
 import sqlite3
+from typing import Optional
 
 db_connection = sqlite3.connect(":memory:")
 db_connection.isolation_level = None
@@ -17,3 +18,19 @@ def init_db() -> None:
 
 
 init_db()
+
+
+def insert_client_mapping(client_id: str, player_id: str, room_id: str = None) -> None:
+    cur = db_connection.cursor()
+    cur.execute(
+        "INSERT INTO client_player_room VALUES (?, ?, ?)",
+        (client_id, player_id, room_id),
+    )
+
+
+def set_client_room(client_id: str, room_id: Optional[str]) -> None:
+    cur = db_connection.cursor()
+    cur.execute(
+        "UPDATE client_player_room SET room_id=? WHERE client_id = ?",
+        (room_id, client_id),
+    )
