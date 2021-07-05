e9d33da892b226c85a9945a1181a3a41a46190ba
DarkAce65 <tvdood99@gmail.com>
2021-07-05 00:31:36 -0400

    Use SQL type alias for room id

--- database/init/schema.sql
+++ database/init/schema.sql
@@ -1,12 +1,14 @@
+CREATE TYPE room_id CHAR(4);
+
 CREATE TABLE players(id TEXT PRIMARY KEY NOT NULL, name TEXT);
 
 CREATE TABLE rooms(
-  id CHAR(4) PRIMARY KEY NOT NULL,
+  id room_id PRIMARY KEY NOT NULL,
   room_state int NOT NULL
 );
 
 CREATE TABLE room_players(
-  room_id CHAR(4) NOT NULL,
+  room_id room_id NOT NULL,
   player_id TEXT NOT NULL,
   UNIQUE(room_id, player_id),
   FOREIGN KEY(room_id) REFERENCES rooms(id),
@@ -16,7 +18,7 @@ CREATE TABLE room_players(
 CREATE TABLE client_player_room(
   client_id TEXT PRIMARY KEY NOT NULL,
   player_id TEXT NOT NULL,
-  room_id CHAR(4),
+  room_id room_id,
   FOREIGN KEY(player_id) REFERENCES players(id),
   FOREIGN KEY(room_id) REFERENCES rooms(id)
 );
