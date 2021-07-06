f89ccbd1246a34bee411cb9e0ee9b59c72b8d2c5
DarkAce65 <tvdood99@gmail.com>
2021-07-06 11:45:52 -0400

    Capitalize types

--- database/init/schema.sql
+++ database/init/schema.sql
@@ -1,14 +1,14 @@
-CREATE DOMAIN room_id CHAR(4);
+CREATE DOMAIN ROOM_ID CHAR(4);
 
 CREATE TABLE players(id TEXT PRIMARY KEY NOT NULL, name TEXT);
 
 CREATE TABLE rooms(
-  id room_id PRIMARY KEY NOT NULL,
-  room_state int NOT NULL
+  id ROOM_ID PRIMARY KEY NOT NULL,
+  room_state INT NOT NULL
 );
 
 CREATE TABLE room_players(
-  room_id room_id NOT NULL,
+  room_id ROOM_ID NOT NULL,
   player_id TEXT NOT NULL,
   UNIQUE(room_id, player_id),
   FOREIGN KEY(room_id) REFERENCES rooms(id),
@@ -18,7 +18,7 @@ CREATE TABLE room_players(
 CREATE TABLE client_player_room(
   client_id TEXT PRIMARY KEY NOT NULL,
   player_id TEXT NOT NULL,
-  room_id room_id,
+  room_id ROOM_ID,
   FOREIGN KEY(player_id) REFERENCES players(id),
   FOREIGN KEY(room_id) REFERENCES rooms(id)
 );
