6b2cea091dfda96c742ec65f380df817dc3dfea5
DarkAce65 <tvdood99@gmail.com>
2022-01-15 18:15:26 -0500

    Remove needless dependency

--- server/server/models/camel_model.py
+++ server/server/models/camel_model.py
@@ -1,15 +1,19 @@
-from humps import camelize
 from pydantic import BaseModel
 from pydantic.generics import GenericModel
 
 
+def to_camel(string: str) -> str:
+    [head, *tail] = string.split("_")
+    return head + "".join(word.capitalize() for word in tail)
+
+
 class CamelModel(BaseModel):
     class Config:
-        alias_generator = camelize
+        alias_generator = to_camel
         allow_population_by_field_name = True
 
 
 class GenericCamelModel(GenericModel):
     class Config:
-        alias_generator = camelize
+        alias_generator = to_camel
         allow_population_by_field_name = True
