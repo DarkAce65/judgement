f97d4f50da10f9bb72aa8d1786bad60632d8538d
DarkAce65 <tvdood99@gmail.com>
2022-04-17 17:10:54 -0400

    Hand off callback responsibility to parent component

--- client/src/components/game/Hand.tsx
+++ client/src/components/game/Hand.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from 'react';
+import { useMemo } from 'react';
 
 import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 
@@ -8,11 +8,13 @@ import Card, { CardType } from './Card';
 
 interface Props {
   cards: CardType[];
-  onSetCards?: (cards: CardType[]) => void;
+  onReorderCards?: (sourceIndex: number, destinationIndex: number) => void;
   onSelect?: (index: number) => void;
 }
 
-const Hand = ({ cards, onSetCards }: Props) => {
+const Hand = ({ cards, onReorderCards, onSelect }: Props) => {
+  const handId = useMemo(() => `hand-${Math.random().toString(16).slice(2, 8)}`, []);
+
   const { width } = useWindowSize();
   const cardWidth = useMemo(() => Math.max(100, width / 6), [width]);
   const paddingRight = useMemo(
@@ -23,97 +25,61 @@ const Hand = ({ cards, onSetCards }: Props) => {
     [cardWidth, cards.length]
   );
 
-  const handId = useMemo(() => `hand-${Math.random().toString(16).slice(2, 8)}`, []);
-
-  const [selected, setSelected] = useState<CardType | null>(null);
-
   return (
-    <>
-      <DragDropContext
-        onDragEnd={(result) => {
-          if (!onSetCards || !result.destination) {
-            return;
-          }
+    <DragDropContext
+      onDragEnd={(result) => {
+        if (!onReorderCards || !result.destination) {
+          return;
+        }
 
-          const movedCard = cards[result.source.index];
-          const newCards = [
-            ...cards.slice(0, result.source.index),
-            ...cards.slice(result.source.index + 1),
-          ];
-          newCards.splice(result.destination.index, 0, movedCard);
-
-          onSetCards(newCards);
-        }}
-      >
-        <Droppable droppableId={handId} direction="horizontal">
-          {(droppableProvided) => (
-            <div
-              ref={droppableProvided.innerRef}
-              {...droppableProvided.droppableProps}
-              style={{ display: 'flex', justifyContent: 'center', paddingRight }}
-            >
-              {cards.map((card, index) => (
-                <Draggable
-                  key={`${index}${card.suit}${card.rank}`}
-                  draggableId={`${index}${card.suit}${card.rank}`}
-                  index={index}
-                >
-                  {(draggableProvided, snapshot) => (
-                    <div
-                      ref={draggableProvided.innerRef}
-                      {...draggableProvided.draggableProps}
-                      style={{
-                        ...draggableProvided.draggableProps.style,
-                        display: 'inline-block',
-                        ...(!snapshot.isDragging && { width: `${100 / cards.length}%` }),
-                        minWidth: 0.13 * cardWidth,
-                        maxWidth: cardWidth + 10,
-                        textAlign: 'center',
-                      }}
-                    >
-                      <Card
-                        {...draggableProvided.dragHandleProps}
-                        card={card}
-                        style={{ width: cardWidth }}
-                      />
-                    </div>
-                  )}
-                </Draggable>
-              ))}
-              {droppableProvided.placeholder}
-            </div>
-          )}
-        </Droppable>
-      </DragDropContext>
-      {selected && (
-        <div
-          style={{
-            position: 'fixed',
-            display: 'flex',
-            zIndex: 100,
-            top: 0,
-            left: 0,
-            width: '100%',
-            height: '100%',
-            background: '#00000044',
-            boxShadow: 'inset 0 0 100px black',
-            alignItems: 'center',
-          }}
-        >
-          <div style={{ flexGrow: 1 }} />
-          <div style={{ flex: '0 0 auto' }}>
-            <Card
-              card={selected}
-              onClick={() => {
-                setSelected(null);
-              }}
-              style={{ width: cardWidth }}
-            />
+        onReorderCards(result.source.index, result.destination.index);
+      }}
+    >
+      <Droppable droppableId={handId} direction="horizontal">
+        {(droppableProvided) => (
+          <div
+            ref={droppableProvided.innerRef}
+            {...droppableProvided.droppableProps}
+            style={{ display: 'flex', justifyContent: 'center', paddingRight }}
+          >
+            {cards.map((card, index) => (
+              <Draggable
+                key={`${index}${card.suit}${card.rank}`}
+                draggableId={`${index}${card.suit}${card.rank}`}
+                index={index}
+              >
+                {(draggableProvided, snapshot) => (
+                  <div
+                    ref={draggableProvided.innerRef}
+                    {...draggableProvided.draggableProps}
+                    onClick={() => {
+                      if (onSelect) {
+                        onSelect(index);
+                      }
+                    }}
+                    style={{
+                      ...draggableProvided.draggableProps.style,
+                      display: 'inline-block',
+                      ...(!snapshot.isDragging && { width: `${100 / cards.length}%` }),
+                      minWidth: 0.13 * cardWidth,
+                      maxWidth: cardWidth + 10,
+                      textAlign: 'center',
+                    }}
+                  >
+                    <Card
+                      {...draggableProvided.dragHandleProps}
+                      card={card}
+                      style={{ width: cardWidth }}
+                    />
+                  </div>
+                )}
+              </Draggable>
+            ))}
+            {droppableProvided.placeholder}
           </div>
-          <div style={{ flexGrow: 1 }} />
-        </div>
-      )}
-    </>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 };
 
